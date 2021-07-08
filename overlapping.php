eb1994f4b724664b9844e228bc001385fe58bc3e
Mihai24 <56975056+Mihai24@users.noreply.github.com>
2021-07-08 21:36:58 +0300

    final touch

--- overlapping.php
+++ overlapping.php
@@ -1,33 +1,34 @@
 <?php
 
+    $startDate1 = '10-05-2021 10:00:00';
 
-    $startDate1 = '11-04-2021 10:00:00';
-    
-    $endDate1 = '12-04-2021 10:00:00';
+    $endDate1 = '13-05-2021 10:00:00';
 
     $startDate2 = '04-04-2021 21:30:15';
 
-    $endDate2 = '05-04-2021 04:30:00';
+    $endDate2 = '08-05-2021 04:30:00';
+
+    const YEAR = 31556926;
+
+    const MONTH = 2629743;
+
+    const DAY = 86400;
+
+    const HOUR = 3600;
+
+    const MINUTE = 60;
 
     function convertDateTimeInSeconds(string $date): int
     {
-        $year = 31556926;
-
-        $month = 2629743;
-    
-        $day = 86400;
-    
-        $hour = 3600;
-    
-        $minute = 60;
-
+        //Split datetime string in array
         $splitSpaces = explode(' ', $date);
         $splitLines = explode('-', $splitSpaces[0]);
         $splitSeparator = explode(':', $splitSpaces[1]);
         $array = array_merge($splitLines, $splitSeparator);
 
-        return (int)$array[0] * $day + (int)$array[1] * $month + (int)$array[2] * $year + 
-                (int)$array[3] * $hour + (int)$array[4] * $minute + (int)$array[5];
+        //Calculate datetime from array in seconds
+        return (int)$array[0] * DAY + (int)$array[1] * MONTH + (int)$array[2] * YEAR +
+            (int)$array[3] * HOUR + (int)$array[4] * MINUTE + (int)$array[5];
     }
 
     function validateDates(string $startDate, string $endDate): bool
@@ -41,8 +42,8 @@
         if (validateDates($startDate1, $endDate1) == true && validateDates($startDate2, $endDate2) == true)
         {
             //Check dates for overlapping after validation
-            return convertDateTimeInSeconds($endDate1) <= convertDateTimeInSeconds($startDate2) 
-            && convertDateTimeInSeconds($startDate1) >= convertDateTimeInSeconds($endDate2) ? false : true;
+            return convertDateTimeInSeconds($startDate1) <= convertDateTimeInSeconds($endDate2)
+            && convertDateTimeInSeconds($endDate1) >= convertDateTimeInSeconds($startDate2) ? true : false;
         }
 
         return true;
